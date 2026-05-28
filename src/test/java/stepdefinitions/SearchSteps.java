42e01573f4bafce77058d221322619c466a6d4a5
Monika <smonika4566@gmail.com>
2026-05-28 16:48:43 +0530

    course page completed

--- src/test/java/stepdefinitions/SearchSteps.java
+++ src/test/java/stepdefinitions/SearchSteps.java
@@ -1,4 +1,21 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.*;
+import pages.CoursesPage;
+
 public class SearchSteps {
+
+    CoursesPage courses = new CoursesPage();
+
+    // Scenario: Course Search + Filter + Extract
+
+    @Given("User is on homepage")
+    public void user_is_on_homepage() {
+        // Browser already opened via Hooks
+    }
+
+    @When("User performs course search, filter and extraction")
+    public void user_performs_full_flow() {
+        courses.searchFilterAndExtract();   // call single method
+    }
 }
