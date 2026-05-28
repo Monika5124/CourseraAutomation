cef05072d01b7dc12e1f87bddbedda762dc6b61a
muvvajhansilakshmi <jhansilakshmimuvva88@gmail.com>
2026-05-28 10:32:36 +0530

    Browser setup modified

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,4 +1,18 @@
 package hooks;
 
-public class Hooks {
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
+import utils.BaseClass;
+
+public class Hooks extends BaseClass {
+
+    @Before
+    public void start() {
+        setup();
+    }
+
+    @After
+    public void end() {
+        tearDown();
+    }
 }
--- src/test/java/utils/DriverSetup.java
+++ src/test/java/utils/DriverSetup.java
@@ -1,24 +1,57 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverSetup {
 
-    public static WebDriver driver;
+    private static WebDriver driver;
+    private static Properties prop;
+
+    public static WebDriver initDriver() {
+
+        try {
+            prop = new Properties();
+            FileInputStream fis = new FileInputStream("src/test/resources/features/config.properties");
+            prop.load(fis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String browser = prop.getProperty("browser");
+
+        if (driver == null) {
+
+            if (browser.equalsIgnoreCase("chrome")) {
+                driver = new ChromeDriver();
+            } else if (browser.equalsIgnoreCase("edge")) {
+                driver = new EdgeDriver();
+            } else {
+                driver = new ChromeDriver();
+            }
+
+            driver.manage().window().maximize();
+        }
+
+        return driver;
+    }
 
     public static WebDriver getDriver() {
-        if (driver == null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        }
         return driver;
+    }
+
+    public static String getURL() {
+        return prop.getProperty("url");
     }
 
     public static void quitDriver() {
         if (driver != null) {
             driver.quit();
+            driver = null;
         }
     }
 }
